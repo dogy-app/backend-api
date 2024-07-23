62137ecdfe7a67e909b9a8c9317230e07c1dccfa
serfoll <sergiofollyvi@gmail.com>
2024-07-23 13:32:34 +0200

    startup file update

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 # Start the worker in the background
-python watchdog_worker.py &&
+python ./watchdog_worker.py &&
 
 # Start the main application
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
