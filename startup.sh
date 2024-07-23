4592d35aaab330525b19a068a52b8356cc121579
serfoll <sergiofollyvi@gmail.com>
2024-07-23 13:46:37 +0200

    startup file command fix

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 # Start the worker in the background
-python watchdog_workers.py &&
+python watchdog_workers.py &
 
 # Start the main application
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
