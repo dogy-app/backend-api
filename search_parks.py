dedea355b4627c7d04631d369137f6a3d53e2822
Sheape <86521166+Sheape@users.noreply.github.com>
2024-08-07 19:21:32 +0800

    feat(gmaps): added database and existing search

--- search_parks.py
+++ search_parks.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 import requests
 import pygeohash as pgh
+from database import Database
 from helpers import upload_image_to_azure
 
 load_dotenv()
@@ -17,7 +18,7 @@ class SearchParks:
         self.places = []
         self.gmaps = googlemaps.Client(key=api_key)
 
-    def geohash(self, latitude: float, longitude: float) -> str:
+    def geohash(self, longitude: float, latitude: float,) -> str:
         """
         Encode latitude and longitude into a geohash
         :param latitude: The latitude of the location
@@ -165,7 +166,7 @@ class SearchParks:
             'name': place['displayName']['text'],
             'city': location_metadata['city'],
             'country': location_metadata['country'],
-            'geohash': self.geohash(location_metadata['latitude'], location_metadata['longitude']),
+            'geohash': self.geohash(longitude=location_metadata['longitude'], latitude=location_metadata['latitude']),
             'address': place['formattedAddress'],
             'location': [location_metadata['latitude'], location_metadata['longitude']],
             'website_url': place['websiteUri'],
@@ -182,8 +183,43 @@ class SearchParks:
 
         return list(park_details)
 
+    def insert_parks(self, park_details, database):
+        """
+        Insert parks into the database
+        :param database: The database to insert the parks into
+        """
+        database.connect_db().set_collection("places", "dog_parks")
+        database.insert_many(park_details)
+
+    def search_existing_parks(self, location, database, radius=10000):
+        """
+        Search for existing parks in a given location based on latitude and longitude.
+        :param location: The location to search for parks
+        :param radius: The radius (meters) within which to search for parks
+        """
+        location_metadata = self.get_location_metadata_geocode(location)
+        database.connect_db().set_collection("places", "dog_parks")
+        results = database.collection.find({
+            "location": {
+                "$near": {
+                    "$geometry": {
+                        "type": "Point",
+                        "coordinates": [location_metadata['longitude'], location_metadata['latitude']]
+                    },
+                    "$maxDistance": radius
+                }
+            }
+        })
+
+        # for result in results:
+        #     print(result)
+
+        return results
+
 if __name__ == "__main__":
     location = "Stockholm, Sweden"
     search_parks = SearchParks(GOOGLE_API_KEY, location)
     search_results = search_parks.search_new_parks(location).extract_park_details()
+    # search_results = search_parks.search_existing_parks(location, Database(os.getenv("AZURE_COSMOSDB_CONNECTION_STRING")), radius=10000)
+    search_parks.insert_parks(search_results, Database(os.getenv("AZURE_COSMOSDB_CONNECTION_STRING")))
     print(f"Search results: {search_results}")
