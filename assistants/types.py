1ddb2c4246ce955421a925e170b51dd7f5b29f25
Sheape <86521166+Sheape@users.noreply.github.com>
2024-12-18 19:43:09 +0800

    chore: remove routes and code for OpenAI assistant

--- assistants/types.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-
-from pydantic import BaseModel
-
-
-class UserMessage(BaseModel):
-    user_message: str
-    user_name: str
-    thread_id: Optional[str] = None
--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-from contextlib import asynccontextmanager
+# from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
 from starlette.responses import JSONResponse
 
-from database.core import init_db
+# from database.core import init_db
 from database.models import Place, validate_schema_place
-from routers.assistants import router as assistants_router
-from routers.audio import router as audio_router
 from routers.images import router as images_router
 from routers.notifications import router as notifications_router
 from routers.parks import router as parks_router
-
 
 # @asynccontextmanager
 # async def lifespan(_: FastAPI):
@@ -27,9 +24,7 @@ app = FastAPI(
 
 app.include_router(images_router, tags=["images"])
 app.include_router(parks_router, tags=["parks"])
-app.include_router(audio_router, tags=["audio"])
 app.include_router(notifications_router, tags=["notifications"])
-app.include_router(assistants_router, tags=["assistant"])
 
 
 # Entry
