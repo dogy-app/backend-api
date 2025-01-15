2ed9f16f817817f507b312e629020943019f012c
Sheape <86521166+Sheape@users.noreply.github.com>
2025-01-15 13:50:33 +0800

    feat: async support for SQLModel through psycopg3

--- app/database/core.py
+++ app/database/core.py
@@ -1,7 +1,8 @@
+from typing import AsyncGenerator
 
-from azure.identity import EnvironmentCredential
-from sqlalchemy.engine import URL
-from sqlmodel import Session, SQLModel, create_engine
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.config import get_settings
 
@@ -11,31 +12,34 @@ settings = get_settings()
 class NotFoundError(Exception):
     pass
 
+# def get_postgresql_connection_string() -> str:
+#     """
+#     Generate PostgreSQL Connection String based on Azure Managed Identity
+#     """
+#     credential = EnvironmentCredential()
+#     token = credential.get_token(settings.db_token_endpoint)
+#
+#     url = URL.create(
+#         drivername="postgresql+psycopg",
+#         username=settings.postgresql_user,
+#         password=token.token,
+#         host=settings.postgresql_host,
+#         port=5432,
+#         database=settings.postgresql_db_name,
+#     )
+#     return str(url)
 def get_postgresql_connection_string() -> str:
-    """
-    Generate PostgreSQL Connection String based on Azure Managed Identity
-    """
-    credential = EnvironmentCredential()
-    token = credential.get_token(settings.db_token_endpoint)
+    return "postgresql+psycopg://postgres:password@localhost:5432/postgres"
 
-    url = URL.create(
-        drivername="postgresql+psycopg2",
-        username=settings.postgresql_user,
-        password=token.token,
-        host=settings.postgresql_host,
-        port=5432,
-        database=settings.postgresql_db_name,
-    )
-    return str(url)
-
-DATABASE_URL = get_postgresql_connection_string()
-engine = create_engine(DATABASE_URL, echo=True)
+DATABASE_URL: str = get_postgresql_connection_string()
+async_engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=True)
 
 
-def init_db():
-    SQLModel.metadata.create_all(engine)
+async def init_db() -> None:
+    async with async_engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
 
 
-def get_session():
-    with Session(engine) as session:
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSession(async_engine) as session:
         yield session
--- app/main.py
+++ app/main.py
@@ -1,31 +1,32 @@
-# from contextlib import asynccontextmanager
+from contextlib import asynccontextmanager
 
-import uvicorn
-
-# from database.core import init_db
 from fastapi import FastAPI
 from starlette.responses import JSONResponse
 
-from app.database.models import Place, validate_schema_place
-from app.routers.images import router as images_router
-from app.routers.parks import router as parks_router
-from app.routers.pets import router as pets_router
-from app.routers.users import router as users_router
+from .database.core import async_engine, init_db
+from .routers.images import router as images_router
+from .routers.pets import router as pets_router
+from .routers.users import router as users_router
 
-# @asynccontextmanager
-# async def lifespan(_: FastAPI):
-#     init_db()
-#     yield
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    # Startup Sequence
+    await init_db()
+    yield
+    # Shutdown Sequence
+    await async_engine.dispose()
 
 
 app = FastAPI(
     title="Dogy Backend API",
     description="The Backend API for Dogy App",
-    # lifespan=lifespan,
+    lifespan=lifespan,
+    prefix="/api/v1"
 )
 
 app.include_router(images_router, tags=["images"])
-app.include_router(parks_router, tags=["parks"])
+# app.include_router(parks_router, tags=["parks"])
 app.include_router(users_router, tags=["users"])
 app.include_router(pets_router, tags=["pets"])
 # app.include_router(notifications_router, tags=["notifications"])
@@ -35,12 +36,3 @@ app.include_router(pets_router, tags=["pets"])
 @app.get("/")
 def api_entry():
     return JSONResponse(content={"message": "Welcome to the Dogy API!"})
-
-
-@app.post("/validate_schema")
-async def validate_schema_endpoint(park: Place | None = None):
-    validate_schema_place(park)
-    return JSONResponse(content={"result": "success"})
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
