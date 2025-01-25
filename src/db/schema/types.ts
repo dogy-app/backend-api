89b6423b358f2fb5d4c82571d49f1cf4ce505445
Sheape <86521166+Sheape@users.noreply.github.com>
2025-01-25 19:21:43 +0800

    feat: added pets schema

--- src/db/schema/types.ts
+++ src/db/schema/types.ts
@@ -1,5 +1,40 @@
 import { pgEnum } from "drizzle-orm/pg-core";
+import {
+	Gender,
+	PetAggressionLevel,
+	PetAllergy,
+	PetBehavior,
+	PetBreed,
+	PetInteraction,
+	PetPersonality,
+	PetReactivity,
+	PetSize,
+	SubscriptionType,
+	enumToPgEnum,
+} from "./enums";
 
-const genderEnum = pgEnum("gender", ["male", "female"]);
-
-export { genderEnum };
+export const gender = pgEnum("gender", enumToPgEnum(Gender));
+export const subscriptionType = pgEnum(
+	"subscription_type",
+	enumToPgEnum(SubscriptionType),
+);
+export const petSize = pgEnum("pet_size", enumToPgEnum(PetSize));
+export const petBreed = pgEnum("pet_breed", enumToPgEnum(PetBreed));
+export const petAggressionLevel = pgEnum(
+	"pet_aggression_level",
+	enumToPgEnum(PetAggressionLevel),
+);
+export const petAllergy = pgEnum("pet_allergy", enumToPgEnum(PetAllergy));
+export const petBehavior = pgEnum("pet_behavior", enumToPgEnum(PetBehavior));
+export const petInteraction = pgEnum(
+	"pet_interaction",
+	enumToPgEnum(PetInteraction),
+);
+export const petPersonality = pgEnum(
+	"pet_personality",
+	enumToPgEnum(PetPersonality),
+);
+export const petReactivity = pgEnum(
+	"pet_reactivity",
+	enumToPgEnum(PetReactivity),
+);
