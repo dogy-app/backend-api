01d5cd9498ce43b24ec2ecf2751cd8acbd273e38
Sheape <86521166+Sheape@users.noreply.github.com>
2025-02-17 04:32:38 +0800

    chore: cleanup unnecessary typescript files

--- src/db/index.ts
+++
@@ -1,8 +0,0 @@
-import env from "@/env";
-import { drizzle } from "drizzle-orm/node-postgres";
-import * as schema from "./schema";
-
-export const db = drizzle(env.DATABASE_CONNECTION_STRING, { schema });
-
-export type db = typeof db;
-export default db;
--- src/lib/utils.ts
+++
@@ -1,1 +0,0 @@
-export type ExtractBaseType<T, U> = Pick<T, keyof U>;
