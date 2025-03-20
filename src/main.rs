3523c8e400e9292fd0cc68f1841c284a3f16d4ae
Sheape <86521166+Sheape@users.noreply.github.com>
2025-03-20 11:34:04 +0800

    chore: switched from direct connection to pgpool

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::sync::Arc;
+
 use axum::{routing::get, Json, Router};
 use config::load_config;
 use serde_json::json;
 use service::users::routes::root_user_routes;
-use sqlx::{Connection, PgConnection};
+use sqlx::{postgres::PgPoolOptions, Connection, PgConnection, PgPool};
 use tracing_subscriber::EnvFilter;
 
 pub use self::error::{Error, Result};
@@ -11,6 +13,11 @@ mod config;
 mod error;
 mod middleware;
 mod service;
+
+#[derive(Clone)]
+struct AppState {
+    db: Arc<PgPool>,
+}
 
 #[tokio::main]
 async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
@@ -22,6 +29,14 @@ async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
 
     let config = load_config();
     println!("Port: {}", config.PORT);
+    println!("--> Connecting to database...");
+    let pool = PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&config.DATABASE_URL)
+        .await
+        .expect("Failed to create PgPool.");
+    println!("--> Connected to database.");
+
     //let mut conn = PgConnection::connect(&config.DATABASE_URL).await.unwrap();
     //sqlx::migrate!("./migrations").run(&mut conn).await?;
     //let row = sqlx::query("SELECT 1 + 1 AS result")
@@ -31,17 +46,20 @@ async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     //let sum: i32 = row.get("result");
     //
     //println!("result: {}", sum);
+    //
+    let shared_state = AppState { db: Arc::new(pool) };
 
     let app = Router::new()
         .route(
             "/",
             get(|| async { Json(json!({ "message": "Welcome to Dogy API" })) }),
         )
+        .nest("/api/v1", root_user_routes(shared_state.clone()).await)
+        .with_state(shared_state)
         .nest(
             "/api/v1",
             service::healthcheck::routes::healthcheck_routes().await,
-        )
-        .nest("/api/v1", root_user_routes().await);
+        );
 
     // run our app with hyper, listening globally on port 8080
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", config.PORT))
