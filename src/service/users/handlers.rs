1f7932a6f5d77bd8a1996d3cdc3d6a6ab856e52c
Sheape <86521166+Sheape@users.noreply.github.com>
2025-03-20 11:32:54 +0800

    feat: added all routes for users

--- src/service/users/handlers.rs
+++ src/service/users/handlers.rs
@@ -1,29 +1,32 @@
+use axum::extract::State;
 use axum::{extract::Extension, Json};
-use sqlx::postgres::PgConnection;
-use sqlx::Connection;
+use serde_json::{json, Value};
+use sqlx::{Execute, QueryBuilder};
 use uuid::Uuid;
 
 use crate::middleware::auth::layer::CurrentUser;
-use crate::service::users::models::UserNotification;
-use crate::{config::load_config, service::users::models::UserSubscription};
+use crate::service::users::models::JoinedFullUser;
+use crate::AppState;
 
-use super::models::{FullUser, User};
+use super::models::{
+    FullUser, User, UserNotification, UserNotificationUpdate, UserSubscription,
+    UserSubscriptionUpdate, UserUpdate,
+};
 
 pub async fn create_user(
     Extension(current_user): Extension<CurrentUser>,
+    State(state): State<AppState>,
     Json(mut user): Json<FullUser>,
 ) -> Json<FullUser> {
-    let config = load_config();
-    println!("--> Connecting to Neon DB...");
-    let mut conn = PgConnection::connect(&config.DATABASE_URL).await.unwrap();
+    let conn = &*state.db;
     println!("--> Starting transaction...");
     let mut txn = conn.begin().await.unwrap();
 
     // Inserting Base User
-    let user_id: (Uuid, ) = sqlx::query_as(
-        r"
-INSERT INTO users (name, external_id, timezone, gender, has_onboarded) VALUES ($1, $2, $3, $4, $5) RETURNING id;
-",
+    let user_id: (Uuid,) = sqlx::query_as(
+        r#"INSERT INTO users (name, external_id, timezone, gender, has_onboarded)
+        VALUES ($1, $2, $3, $4, $5) RETURNING id;
+        "#,
     )
     .bind(&user.base.name)
     .bind(&current_user.user_id)
@@ -35,9 +38,10 @@ INSERT INTO users (name, external_id, timezone, gender, has_onboarded) VALUES ($
     .unwrap();
 
     sqlx::query(
-        r"
-INSERT INTO user_subscriptions (user_id, trial_start_date, subscription_type, is_trial_mode) VALUES ($1, $2, $3, $4);
-",
+        r#"INSERT
+        INTO user_subscriptions (user_id, trial_start_date, subscription_type, is_trial_mode)
+        VALUES ($1, $2, $3, $4);
+        "#,
     )
     .bind(user_id.0)
     .bind(user.subscription.trial_start_date)
@@ -48,10 +52,10 @@ INSERT INTO user_subscriptions (user_id, trial_start_date, subscription_type, is
     .unwrap();
 
     sqlx::query(
-        r"
-INSERT INTO user_notifications (user_id, enabled, is_registered, daily_enabled, playtime_enabled) VALUES ($1, $2, $3, $4, $5);
-;
-",
+        r#"INSERT
+        INTO user_notifications (user_id, enabled, is_registered, daily_enabled, playtime_enabled)
+        VALUES ($1, $2, $3, $4, $5);
+        "#,
     )
     .bind(user_id.0)
     .bind(user.notifications.enabled)
@@ -70,39 +74,166 @@ INSERT INTO user_notifications (user_id, enabled, is_registered, daily_enabled, 
     Json(user)
 }
 
-pub async fn get_user(Extension(current_user): Extension<CurrentUser>) -> Json<FullUser> {
+pub async fn get_user(
+    State(state): State<AppState>,
+    Extension(current_user): Extension<CurrentUser>,
+) -> Json<FullUser> {
     println!("--> GET /users test");
-    let query = r"
-    SELECT u.*, us.trial_start_date,
-    us.subscription_type,
-    us.is_trial_mode,
-    un.enabled,
-    un.is_registered,
-    un.daily_enabled,
-    un.playtime_enabled
+    let conn = &*state.db;
+    let query = r#"
+    SELECT u.*, us.trial_start_date, us.subscription_type, us.is_trial_mode,
+        un.enabled, un.is_registered, un.daily_enabled, un.playtime_enabled
     FROM users u
     LEFT JOIN user_subscriptions us ON u.id = us.user_id
     LEFT JOIN user_notifications un ON u.id = un.user_id
     WHERE u.id = $1;
-    ";
+    "#;
 
-    let config = load_config();
-    println!("--> Connecting to Neon DB...");
-    let mut conn = PgConnection::connect(&config.DATABASE_URL).await.unwrap();
     println!("--> Starting transaction...");
 
-    let (user, user_subscription, user_notifications) =
-        sqlx::query_as::<_, (User, UserSubscription, UserNotification)>(query)
-            .bind(current_user.internal_id.unwrap())
-            .fetch_one(&mut conn)
-            .await
-            .unwrap();
+    let user_info = sqlx::query_as::<_, JoinedFullUser>(query)
+        .bind(current_user.internal_id.unwrap())
+        .fetch_one(conn)
+        .await
+        .unwrap();
 
     let full_user = FullUser {
-        base: user,
-        subscription: user_subscription,
-        notifications: user_notifications,
+        base: User {
+            external_id: user_info.external_id,
+            name: user_info.name,
+            timezone: user_info.timezone,
+            gender: user_info.gender,
+            has_onboarded: user_info.has_onboarded,
+        },
+        notifications: UserNotification {
+            enabled: user_info.enabled,
+            is_registered: user_info.is_registered,
+            daily_enabled: user_info.daily_enabled,
+            playtime_enabled: user_info.playtime_enabled,
+        },
+        subscription: UserSubscription {
+            trial_start_date: user_info.trial_start_date,
+            subscription_type: user_info.subscription_type,
+            is_trial_mode: user_info.is_trial_mode,
+        },
     };
 
     Json(full_user)
+}
+
+pub async fn update_user_base(
+    State(state): State<AppState>,
+    Extension(current_user): Extension<CurrentUser>,
+    Json(user): Json<UserUpdate>,
+) -> Json<UserUpdate> {
+    let conn = &*state.db;
+    let query = r#"
+    UPDATE users
+    SET
+        name = COALESCE($2, name),
+        timezone = COALESCE($3, timezone),
+        gender = COALESCE($4, gender),
+        has_onboarded = COALESCE($5, has_onboarded)
+    WHERE id = $1;
+        "#;
+
+    sqlx::query(query)
+        .bind(current_user.internal_id.unwrap())
+        .bind(&user.name)
+        .bind(&user.timezone)
+        .bind(&user.gender)
+        .bind(user.has_onboarded)
+        .execute(conn)
+        .await
+        .unwrap();
+
+    Json(user)
+}
+
+pub async fn update_user_subscription(
+    State(state): State<AppState>,
+    Extension(current_user): Extension<CurrentUser>,
+    Json(user_sub): Json<UserSubscriptionUpdate>,
+) -> Json<UserSubscriptionUpdate> {
+    println!("--> Updating user subscription");
+    let conn = &*state.db;
+    let mut query_builder = QueryBuilder::new(
+        r#"
+        UPDATE user_subscriptions SET
+        subscription_type = COALESCE(
+        "#,
+    );
+
+    query_builder
+        .push_bind(&user_sub.subscription_type)
+        .push(r#", subscription_type), is_trial_mode = COALESCE( "#)
+        .push_bind(user_sub.is_trial_mode)
+        .push(", is_trial_mode) ");
+
+    println!("--> User Subscription: {:?}", user_sub.trial_start_date);
+
+    if let Some(trial_date) = user_sub.trial_start_date {
+        match trial_date {
+            Some(date) => {
+                query_builder.push(",trial_start_date = ").push_bind(date);
+            }
+            None => {
+                query_builder.push(",trial_start_date = NULL");
+            }
+        }
+    }
+
+    query_builder
+        .push("\nWHERE user_id = ")
+        .push_bind(current_user.internal_id.unwrap())
+        .push(";");
+
+    let query = query_builder.build();
+    println!("--> Query: {}", query.sql());
+    query.execute(conn).await.unwrap();
+
+    Json(user_sub)
+}
+
+pub async fn update_user_notification(
+    State(state): State<AppState>,
+    Extension(current_user): Extension<CurrentUser>,
+    Json(user_notif): Json<UserNotificationUpdate>,
+) -> Json<UserNotificationUpdate> {
+    let conn = &*state.db;
+    let query = r#"
+    UPDATE user_notifications
+    SET
+        enabled = COALESCE($2, enabled),
+        is_registered = COALESCE($3, is_registered),
+        daily_enabled = COALESCE($4, daily_enabled),
+        playtime_enabled = COALESCE($5, playtime_enabled)
+    WHERE user_id = $1;
+    "#;
+
+    sqlx::query(query)
+        .bind(current_user.internal_id.unwrap())
+        .bind(user_notif.enabled)
+        .bind(user_notif.is_registered)
+        .bind(user_notif.daily_enabled)
+        .bind(user_notif.playtime_enabled)
+        .execute(conn)
+        .await
+        .unwrap();
+
+    Json(user_notif)
+}
+
+pub async fn delete_user(
+    State(state): State<AppState>,
+    Extension(current_user): Extension<CurrentUser>,
+) -> Json<Value> {
+    let conn = &*state.db;
+    sqlx::query("DELETE FROM users WHERE id = $1;")
+        .bind(current_user.internal_id.unwrap())
+        .execute(conn)
+        .await
+        .unwrap();
+
+    Json(json!({ "message": "User deleted successfully" }))
 }
--- src/service/users/routes.rs
+++ src/service/users/routes.rs
@@ -1,15 +1,43 @@
-use axum::{middleware, routing::get, Router};
+use axum::{middleware, routing::get, routing::patch, Router};
 
-use crate::middleware::auth::layer::auth_middleware;
+use crate::{
+    middleware::auth::layer::{auth_middleware, get_internal_id},
+    AppState,
+};
 
-use super::handlers::{create_user, get_user};
+use super::handlers::{
+    create_user, delete_user, get_user, update_user_base, update_user_notification,
+    update_user_subscription,
+};
 
-pub async fn user_routes() -> Router {
+pub async fn user_routes(app_state: AppState) -> Router<AppState> {
     Router::new()
-        .route("/", get(get_user).post(create_user))
+        .route(
+            "/",
+            get(get_user)
+                .delete(delete_user)
+                .patch(update_user_base)
+                .route_layer(middleware::from_fn_with_state(
+                    app_state.clone(),
+                    get_internal_id,
+                ))
+                .post(create_user),
+        )
+        .route(
+            "/subscriptions",
+            patch(update_user_subscription).route_layer(middleware::from_fn_with_state(
+                app_state.clone(),
+                get_internal_id,
+            )),
+        )
+        .route(
+            "/notifications",
+            patch(update_user_notification)
+                .route_layer(middleware::from_fn_with_state(app_state, get_internal_id)),
+        )
         .route_layer(middleware::from_fn(auth_middleware))
 }
 
-pub async fn root_user_routes() -> Router {
-    Router::new().nest("/users", user_routes().await)
+pub async fn root_user_routes(app_state: AppState) -> Router<AppState> {
+    Router::new().nest("/users", user_routes(app_state).await)
 }
