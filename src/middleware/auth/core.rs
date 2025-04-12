310a475e527d8e22912fc1ca7aa346b7a18baa91
Sheape <86521166+Sheape@users.noreply.github.com>
2025-04-12 09:02:44 +0800

    refactor: added config for testing

--- src/middleware/auth/core.rs
+++ src/middleware/auth/core.rs
@@ -45,15 +45,16 @@ pub fn authenticate_user(auth_header: &str) -> Result<CurrentUser> {
     }
 }
 
+#[cfg(test)]
 mod test {
-    #[allow(unused_imports)]
     use super::decode_jwt;
-    #[allow(unused_imports)]
+    use crate::config::load_test_config;
     use jsonwebtoken::Algorithm;
 
     #[test]
     fn test_decode_jwt_ok() {
-        let jwt_token = "eyJhbGciOiJSUzI1NiIsImNhdCI6ImNsX0I3ZDRQRDIyMkFBQSIsImtpZCI6Imluc18ycnN0V2dNOGN2SlltZVloMXZwTU95WWo4NnYiLCJ0eXAiOiJKV1QifQ.eyJhenAiOiJodHRwczovL25lYXQtY2F0LTEyLmFjY291bnRzLmRldiIsImV4cCI6MjA1NzcwOTU3OCwiaWF0IjoxNzQyMzQ5NTc4LCJpc3MiOiJodHRwczovL25lYXQtY2F0LTEyLmNsZXJrLmFjY291bnRzLmRldiIsImp0aSI6IjIxMDgxODQwMDI1NmY4OWE4NjIyIiwibmJmIjoxNzQyMzQ5NTczLCJyb2xlIjpudWxsLCJzdWIiOiJ1c2VyXzJydUhTWEN6ZklScmVSMnRwdHRWUUJsNTEyYSJ9.U3RITdKOE_MN78aIP8IG7aYcrVlGhq_I2wnpgpsHBgNVKWnKXYtymquOSv0taszAI7n3Ju_MyA896xjOIjaTIj3rjmV1-5XNK35kubT1GEiDmM1HSTN0jd4snE8jIW1F3TgqCd89ZdVlxo5FwTUqZ877xmOcJkOjyTq9W9wLK6Ep_pjW6uuoVzq4vFcFMKZV7aOd2g3aZ4LbLTMlIxjpAg3n1FnKNZsfwp3gBu-h5cxB5mn73_Ak4B9nLP0ti-NrrVeHEmesT68GZzOwu4cGF4Gjxy_BFSO64ruJwrMtB1ZAMztve2gHHvdvvIv0caaCp4lejnUhgNhSVzbEP8iLMg";
+        let test_config = load_test_config();
+        let jwt_token = test_config.JWT_TOKEN.as_str();
         let token_data = decode_jwt(jwt_token).unwrap();
         assert_eq!(token_data.header.alg, Algorithm::RS256);
         assert_eq!(token_data.header.typ, Some("JWT".to_string()));
