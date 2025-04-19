769df5ebcc1380d17527d7e20111e9aab0ffc16c
Sheape <86521166+Sheape@users.noreply.github.com>
2025-04-19 17:25:24 +0800

    test: added unit test for config

--- src/config.rs
+++ src/config.rs
@@ -5,7 +5,9 @@ use std::sync::OnceLock;
 use tracing::warn;
 
 pub fn load_config() -> &'static Config {
+    #[cfg(not(test))]
     dotenv().ok();
+
     static INSTANCE: OnceLock<Config> = OnceLock::new();
 
     INSTANCE.get_or_init(|| {
@@ -13,30 +15,14 @@ pub fn load_config() -> &'static Config {
     })
 }
 
-#[cfg(test)]
-pub fn load_test_config() -> &'static TestConfig {
-    dotenv::from_filename(".env.test").ok();
-    static INSTANCE: OnceLock<TestConfig> = OnceLock::new();
-
-    INSTANCE.get_or_init(|| {
-        TestConfig::from_env()
-            .unwrap_or_else(|e| panic!("FATAL: Failed loading env variable. {:?}", e))
-    })
-}
-
 #[allow(non_snake_case)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Config {
     pub DATABASE_URL: String,
     pub LANGGRAPH_ASSISTANT_ENDPOINT: String,
     pub CLERK_RSA_MODULUS: String,
     pub CLERK_RSA_EXPONENT: String,
     pub PORT: String,
-}
-
-#[allow(non_snake_case)]
-#[cfg(test)]
-pub struct TestConfig {
-    pub JWT_TOKEN: String,
 }
 
 impl Config {
@@ -51,15 +37,6 @@ impl Config {
     }
 }
 
-#[cfg(test)]
-impl TestConfig {
-    fn from_env() -> Result<Self> {
-        Ok(Self {
-            JWT_TOKEN: get_env("JWT_TOKEN")?,
-        })
-    }
-}
-
 fn get_env(key: &'static str) -> Result<String> {
     env::var(key).map_err(|_| Error::ConfigMissingEnv(key))
 }
@@ -70,4 +47,115 @@ fn get_env_opt(key: &str, default: &str) -> String {
         key, default
     );
     env::var(key).unwrap_or_else(|_| default.to_string())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::env;
+
+    #[test]
+    fn test_get_env_ok() {
+        // In Rust 2024 Edition, env::set_var is considered unsafe.
+        unsafe {
+            env::set_var("DATABASE_URL", "test_url");
+        }
+        let database_url = get_env("DATABASE_URL").unwrap();
+        assert_eq!(database_url, "test_url".to_string());
+    }
+
+    #[test]
+    fn test_get_env_config_missing_err() {
+        let database_url = get_env("DATABASE_URL");
+        assert!(
+            matches!(database_url, Err(Error::ConfigMissingEnv("DATABASE_URL"))),
+            "Expected ConfigMissingEnv error"
+        );
+    }
+
+    #[test]
+    fn test_get_env_opt_default() {
+        let database_url = get_env_opt("DATABASE_URL", "test_url");
+        assert_eq!(database_url, "test_url".to_string());
+    }
+
+    #[test]
+    fn test_get_env_opt_override() {
+        unsafe {
+            env::set_var("DATABASE_URL", "override_test_url");
+        }
+        let database_url = get_env_opt("DATABASE_URL", "test_url");
+        assert_eq!(database_url, "override_test_url".to_string());
+    }
+
+    #[test]
+    fn test_config_from_env_ok() {
+        unsafe {
+            env::set_var("DATABASE_URL", "test_db_url");
+            env::set_var("LANGGRAPH_ASSISTANT_ENDPOINT", "test_assistant_url");
+            env::set_var("CLERK_RSA_MODULUS", "test_modulus");
+            env::set_var("CLERK_RSA_EXPONENT", "test_exponent");
+        }
+
+        let config = Config::from_env().unwrap();
+        assert_eq!(
+            config,
+            Config {
+                PORT: "8080".to_string(),
+                CLERK_RSA_EXPONENT: "test_exponent".to_string(),
+                CLERK_RSA_MODULUS: "test_modulus".to_string(),
+                LANGGRAPH_ASSISTANT_ENDPOINT: "test_assistant_url".to_string(),
+                DATABASE_URL: "test_db_url".to_string(),
+            }
+        );
+    }
+
+    #[test]
+    fn test_config_from_env_missing_db_url_err() {
+        unsafe {
+            env::set_var("LANGGRAPH_ASSISTANT_ENDPOINT", "test_assistant_url");
+            env::set_var("CLERK_RSA_MODULUS", "test_modulus");
+            env::set_var("CLERK_RSA_EXPONENT", "test_exponent");
+        }
+
+        let config = Config::from_env();
+        assert!(
+            matches!(config, Err(Error::ConfigMissingEnv("DATABASE_URL"))),
+            "Expected ConfigMissingEnv error"
+        );
+    }
+
+    #[test]
+    fn test_load_config_ok() {
+        unsafe {
+            env::set_var("DATABASE_URL", "test_db_url");
+            env::set_var("LANGGRAPH_ASSISTANT_ENDPOINT", "test_assistant_url");
+            env::set_var("CLERK_RSA_MODULUS", "test_modulus");
+            env::set_var("CLERK_RSA_EXPONENT", "test_exponent");
+        }
+
+        let config = load_config();
+        assert_eq!(
+            config,
+            &Config {
+                PORT: "8080".to_string(),
+                CLERK_RSA_EXPONENT: "test_exponent".to_string(),
+                CLERK_RSA_MODULUS: "test_modulus".to_string(),
+                LANGGRAPH_ASSISTANT_ENDPOINT: "test_assistant_url".to_string(),
+                DATABASE_URL: "test_db_url".to_string(),
+            }
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_load_config_panic() {
+        unsafe {
+            env::set_var("LANGGRAPH_ASSISTANT_ENDPOINT", "test_assistant_url");
+            env::set_var("CLERK_RSA_MODULUS", "test_modulus");
+            env::set_var("CLERK_RSA_EXPONENT", "test_exponent");
+        }
+
+        let _ = load_config();
+    }
 }
