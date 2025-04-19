9e83a556f430b432cbcc5e6473980a921faefb8c
Sheape <86521166+Sheape@users.noreply.github.com>
2025-04-19 17:22:31 +0800

    test: added unit and integration test for auth middleware

--- src/middleware/auth/layer.rs
+++ src/middleware/auth/layer.rs
@@ -13,7 +13,7 @@ use crate::{Error, Result};
 /// Represents the metadata about the current user.
 ///
 /// This is passed throughout the middlewares and also handlers.
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct CurrentUser {
     /// Clerk User ID of the current user.
     pub user_id: String,
@@ -79,7 +79,206 @@ pub async fn get_internal_id(
         internal_id: Some(internal_id),
         ..current_user
     };
-    debug!("Current user: {:?}", updated_user);
+    debug!("Current user: {:?}", &updated_user);
     req.extensions_mut().insert(updated_user);
     Ok(next.run(req).await)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::middleware::{log::layer::log_middleware, test::*};
+    use axum::middleware;
+    use axum_test::TestServer;
+    use reqwest::StatusCode;
+    use serde_json::json;
+    use std::env;
+    use testcontainers::ContainerAsync;
+    use testcontainers_modules::postgres::Postgres;
+
+    #[cfg(test)]
+    fn setup_test_server_with_auth() -> TestServer {
+        let test_route = setup_test_router()
+            .route_layer(middleware::from_fn(auth_middleware))
+            .layer(middleware::map_response(log_middleware));
+
+        TestServer::new(test_route).unwrap()
+    }
+
+    #[cfg(test)]
+    fn setup_test_server_with_assert_user() -> TestServer {
+        let test_route = setup_test_router()
+            .layer(middleware::from_fn(assert_current_user_in_extensions_mw))
+            .route_layer(middleware::from_fn(auth_middleware))
+            .layer(middleware::map_response(log_middleware));
+
+        TestServer::new(test_route).unwrap()
+    }
+
+    #[cfg(test)]
+    async fn setup_test_server_with_state(
+        create_user: bool,
+    ) -> (TestServer, ContainerAsync<Postgres>) {
+        let (state, container) = setup_test_db().await;
+        if create_user {
+            sqlx::query(
+                r#"INSERT INTO users (name, external_id, timezone, gender, has_onboarded)
+                VALUES ('Test User', 'user_2ruHSXCzfIRreR2tpttVQBl512a', 'Europe/Stockholm', 'male', true);
+                "#,
+            )
+            .execute(&*state.db)
+            .await
+            .unwrap();
+        }
+        let test_route = setup_test_router()
+            .layer(middleware::from_fn(assert_current_user_full_in_ext_mw))
+            .layer(middleware::from_fn_with_state(state, get_internal_id))
+            .layer(middleware::from_fn(auth_middleware))
+            .layer(middleware::map_response(log_middleware));
+
+        println!(
+            "host: {}",
+            container.get_host_port_ipv4(5432).await.unwrap()
+        );
+
+        (TestServer::new(test_route).unwrap(), container)
+    }
+
+    #[cfg(test)]
+    async fn assert_current_user_in_extensions_mw(req: Request, next: Next) -> Result<Response> {
+        let current_user = req.extensions().get::<CurrentUser>().unwrap();
+        assert_eq!(current_user.user_id, "user_2ruHSXCzfIRreR2tpttVQBl512a");
+        assert_eq!(current_user.role, None);
+        assert_eq!(current_user.internal_id, None);
+        Ok(next.run(req).await)
+    }
+
+    #[cfg(test)]
+    async fn assert_current_user_full_in_ext_mw(req: Request, next: Next) -> Result<Response> {
+        let current_user = req.extensions().get::<CurrentUser>().unwrap();
+        assert_eq!(current_user.user_id, "user_2ruHSXCzfIRreR2tpttVQBl512a");
+        assert_eq!(current_user.role, None);
+        assert!(current_user.internal_id.is_some());
+        Ok(next.run(req).await)
+    }
+
+    #[tokio::test]
+    async fn test_auth_middleware_ok() {
+        let app = setup_test_server_with_auth();
+        let _ = dotenv::from_filename(".env.test");
+        let jwt_token = env::var("JWT_TOKEN").unwrap();
+
+        let response = app
+            .get("/")
+            .add_header("Authorization", format!("Bearer {}", jwt_token))
+            .await;
+
+        response.assert_status(StatusCode::OK);
+        response.assert_text("Middleware test succeeded");
+    }
+
+    #[tokio::test]
+    async fn test_auth_middleware_missing_auth_header_err() {
+        let app = setup_test_server_with_auth();
+
+        let response = app.get("/").await;
+
+        response.assert_status(StatusCode::UNAUTHORIZED);
+        response.assert_header("content-type", "application/json");
+        response.assert_json(&json!({
+            "status": "error",
+            "code": "MISSING_AUTH_HEADER"
+        }));
+    }
+
+    #[tokio::test]
+    async fn test_auth_middleware_no_bearer_prefix_err() {
+        let app = setup_test_server_with_auth();
+
+        let response = app.get("/").add_header("Authorization", "some_token").await;
+
+        response.assert_status(StatusCode::UNAUTHORIZED);
+        response.assert_header("content-type", "application/json");
+        response.assert_json(&json!({
+            "status": "error",
+            "code": "NO_BEARER_PREFIX"
+        }));
+    }
+
+    #[tokio::test]
+    async fn test_auth_middleware_invalid_token_err() {
+        let app = setup_test_server_with_auth();
+
+        let response = app
+            .get("/")
+            .add_header("Authorization", "Bearer invalid_token")
+            .await;
+
+        response.assert_status(StatusCode::UNAUTHORIZED);
+        response.assert_header("content-type", "application/json");
+        response.assert_json(&json!({
+            "status": "error",
+            "code": "INVALID_CREDENTIALS"
+        }));
+    }
+
+    #[tokio::test]
+    async fn test_auth_middleware_current_user_ext_ok() {
+        let app = setup_test_server_with_assert_user();
+        let _ = dotenv::from_filename(".env.test");
+        let jwt_token = env::var("JWT_TOKEN").unwrap();
+
+        let response = app
+            .get("/")
+            .add_header("Authorization", format!("Bearer {}", jwt_token))
+            .await;
+
+        response.assert_status(StatusCode::OK);
+        response.assert_text("Middleware test succeeded");
+    }
+
+    #[tokio::test]
+    async fn test_get_internal_id_mw_ok() {
+        let (app, container) = setup_test_server_with_state(true).await;
+        let _ = dotenv::from_filename(".env.test");
+        let jwt_token = env::var("JWT_TOKEN").unwrap();
+
+        let response = app
+            .get("/")
+            .add_header("Authorization", format!("Bearer {}", jwt_token))
+            .await;
+
+        response.assert_status(StatusCode::OK);
+        response.assert_text("Middleware test succeeded");
+        dbg!(
+            "Host: {}",
+            container.get_host_port_ipv4(5432).await.unwrap()
+        );
+    }
+
+    #[tokio::test]
+    async fn test_get_internal_id_mw_user_not_found_err() {
+        let (app, container) = setup_test_server_with_state(false).await;
+        let _ = dotenv::from_filename(".env.test");
+        let jwt_token = env::var("JWT_TOKEN").unwrap();
+
+        let response = app
+            .get("/")
+            .add_header("Authorization", format!("Bearer {}", jwt_token))
+            .await;
+
+        response.assert_status(StatusCode::NOT_FOUND);
+        response.assert_header("content-type", "application/json");
+        response.assert_json(&json!({
+            "status": "error",
+            "code": "USER_NOT_FOUND",
+            "details": {
+                "user_id": "user_2ruHSXCzfIRreR2tpttVQBl512a"
+            }
+        }));
+        dbg!(
+            "Host: {}",
+            container.get_host_port_ipv4(5432).await.unwrap()
+        );
+    }
 }
