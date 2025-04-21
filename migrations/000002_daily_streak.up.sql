9deb215d97169718d48b08ab41ce5e5d8782cf66
Sheape <86521166+Sheape@users.noreply.github.com>
2025-04-21 09:24:50 +0800

    refactor: renamed daily streaks to challenges

--- migrations/000002_daily_streak.up.sql
+++ migrations/000002_daily_streak.up.sql
@@ -1,12 +1,12 @@
-CREATE TABLE IF NOT EXISTS "user_daily_streaks" (
+CREATE TABLE IF NOT EXISTS user_daily_challenges (
   "id" UUID PRIMARY KEY DEFAULT uuid_generate_v7 (),
-  "created_at" TIMESTAMPTZ,
-  "updated_at" TIMESTAMPTZ,
+  "created_at" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
   "user_id" UUID NOT NULL,
   "challenge" TEXT NOT NULL
 );
 
-CREATE TRIGGER trigger_set_updated_at_on_user_daily_streaks BEFORE
-UPDATE ON "user_daily_streaks" FOR EACH ROW EXECUTE FUNCTION set_updated_at ();
+CREATE TRIGGER trigger_set_updated_at_on_user_daily_challenges BEFORE
+UPDATE ON "user_daily_challenges" FOR EACH ROW EXECUTE FUNCTION set_updated_at ();
 
-ALTER TABLE "user_daily_streaks" ADD CONSTRAINT "user_daily_streaks_user_id_users_id" FOREIGN KEY ("user_id") REFERENCES "public"."users" ("id") ON DELETE cascade ON UPDATE no action;
+ALTER TABLE "user_daily_challenges" ADD CONSTRAINT "user_daily_challenges_user_id_users_id" FOREIGN KEY ("user_id") REFERENCES "public"."users" ("id") ON DELETE cascade ON UPDATE no action;
